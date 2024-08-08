6bd57f70d97f8df5ba23eb6f46aa6de3a98e16d0
Héctor José Díaz Sandate <hectorjosediazsandate@gmail.com>
2024-08-08 00:48:43 -0600

    correo update2

--- app.js
+++ app.js
@@ -65,13 +65,13 @@ const transporter = nodemailer.createTransport({
 console.log(EMAIL_PASSWORD )
 
 app.post('/enviar-correo', (req, res) => {
-  const { titulo, agremiado, fecha, CURP, numero } = req.body;
+  const { titulo, agremiado, fecha, CURP, numero, solicitud } = req.body;
 
   const mailOptions = {
     from: 'hectorjosediazsandate@gmail.com',
     to: 'hector_3127210071@utd.edu.mx',
     subject: `Solicitud de Beneficio: ${titulo}`,
-    text: `El agremiado ${agremiado}, ha solicitado el beneficio: ${titulo} el día ${fecha}.
+    text: `El agremiado ${agremiado}, requiere de ${solicitud}:${titulo} el día ${fecha}.
           CURP: ${CURP}
           Numero: ${numero}`,
   };
