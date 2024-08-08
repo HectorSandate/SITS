eb72d5f52d40609133c37433c1c488dede692296
Héctor José Díaz Sandate <hectorjosediazsandate@gmail.com>
2024-08-08 00:02:16 -0600

    beneficio

--- controller/beneficio.controller.js
+++ controller/beneficio.controller.js
@@ -3,15 +3,15 @@ import Beneficio from '../models/beneficio.model.js';
 // Crear un nuevo beneficio
 export const agregarBeneficio = async (req, res) => {
     try {
-      const { titulo, descripcion } = req.body;
+      const { titulo, categoria, subTitulo, descripcion } = req.body;
   
-      if (!titulo || !descripcion) {
+      if (!titulo || !categoria) {
         return res.status(400).json({ message: "Faltan campos obligatorios: titulo y descripcion" });
       }
   
       let nuevoBeneficio;
       try {
-        nuevoBeneficio = new Beneficio({ titulo, descripcion });
+        nuevoBeneficio = new Beneficio({ titulo, categoria, subTitulo, descripcion });
       } catch (error) {
         return res.status(500).json({ message: "Error al crear la instancia de Beneficio.", error });
       }
@@ -32,7 +32,7 @@ export const agregarBeneficio = async (req, res) => {
 // Obtener todos los beneficios
 export const obtenerBeneficios = async (req, res) => {
   try {
-    const beneficios = await Beneficio.find({}, 'titulo descripcion'); // Select only titulo and descripcion fields
+    const beneficios = await Beneficio.find({}, 'titulo categoria subTitulo descripcion'); // Select only titulo and descripcion fields
     res.status(200).json(beneficios);
   } catch (error) {
     res.status(500).json({ message: "Error al obtener los beneficios.", error });
@@ -43,10 +43,10 @@ export const obtenerBeneficios = async (req, res) => {
 export const actualizarBeneficio = async (req, res) => {
   try {
     const { id } = req.params;
-    const { titulo, descripcion } = req.body;
+    const { titulo, categoria, subTitulo, descripcion } = req.body;
     const beneficioActualizado = await Beneficio.findByIdAndUpdate(
       id,
-      { titulo, descripcion },
+      { titulo, categoria, subTitulo, descripcion },
       { new: true }
     );
     if (!beneficioActualizado) {
--- models/beneficio.model.js
+++ models/beneficio.model.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 const BeneficioSchema = new mongoose.Schema(
   {
     titulo: { type: String, required: true },
-    descripcion: { type: String, required: true },
+    subTitulo: { type: String, required: false },
+    categoria: { type: String, required: true },
+    descripcion: { type: String, required: false },
   },
   {
     timestamps: true, // Añade createdAt y updatedAt automáticamente
